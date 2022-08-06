ad2510c40814050fca6c30e168237fe25532ead8
juanes <juanesgym2018@gmail.com>
2022-08-06 17:29:02 -0500

    haciendo los componetes,mover a cada uno de los nabvar sin recargarse con react-router-dom

---
+++ src/screens/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="container">
+      <h3 className="text-center text-uppercase py-4">About us</h3>
+      <div className="text-center border border-Dark p-4 fst-italic">
+        <p>
+          Estudiante de ingeniería de sistemas, destacado por mi actitud
+          servicial, por mi desempeño académico en la Universidad del Magdalena,
+          me caracterizo por ser una persona que aprende rápidamente y se
+          esfuerza por poner en práctica el conocimiento en áreas como la
+          programación conociendo todas las estructuras de datos necesarias para
+          desempeñar cualquier proyecto. Me encuentro en búsqueda de mi primera
+          oportunidad laboral para demostrar mis capacidades. El desarrollo de
+          software y todos sus componentes me apasionan, por lo anterior quiero
+          iniciar mi carrera laboral y profesional. Capacitado para desarrollo
+          de software en lenguajes como Java, JavaScript, Python, C, HTML, CSS y
+          manejo de base de datos con MySQL o PostgreSQL.
+        </p>
+        <p>
+          • Diseño Web con HTML5 y CSS – junio 2020 
+          • Bootstrap 4 - junio 2020 
+          • Python 3 - junio 2022 
+          • Curso profesional de GIT y GITHUB –junio 2020
+          • Comandos en Linux – junio 2022 
+          • Introducción a la seguridad cibernética –junio 2020 
+          • Fundamentos de Ingeniería de Software - mayo 2020
+        </p>
+      </div>
+    </div>
+  );
+};
