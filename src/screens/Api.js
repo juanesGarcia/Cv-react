06d573795b8690a46cc77bcc26f85b95e2dabad3
juanes <juanesgym2018@gmail.com>
2022-08-10 00:17:46 -0500

    acomodando icon de tareas

--- src/screens/Api.js
+++ src/screens/Api.js
@@ -1,29 +1,29 @@
-
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import * as Apis from "./FunApi";
 import imt from "../img/tareas.png";
 
-//chakra para trabajar con componentes como heading o cards para ue sea mas sencillo 
+//chakra para trabajar con componentes como heading o cards para ue sea mas sencillo
 
-export const Api=()=>{
-    const [Api, setApi] = useState([]);
- 
-    useEffect(() => {
-      Apis.getTask().then(setApi);
-    }, []);
+export const Api = () => {
+  const [Api, setApi] = useState([]);
+
+  useEffect(() => {
+    Apis.getTask().then(setApi);
+  }, []);
   return (
     <div>
-      <img src={imt}/>
-        <h1 className='ps-4 pt-5'>lista de tareas</h1>
-        <ul className='m-4'>
-          {Api.map((task)=>(
-            <li key={task.id} className="mb-2">
-              {task.title}
-            </li>
-          ))}
-        
-  </ul>
+      <h1 className="ps-4 pt-5">
+        <img src={imt} width={30} className="me-2 ms-4 mb-1" />
+        lista de tareas
+      </h1>
+      <ul className="m-4">
+        {Api.map((task) => (
+          <li key={task.id} className="mb-2">
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
