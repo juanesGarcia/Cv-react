7db6b1e2211cd5981c212266f761d60eb00a5fb2
juanes <juanesgym2018@gmail.com>
2022-08-10 12:34:19 -0500

    arreglando errores de sintaxis

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ export const NavBar = () => {
       <form className="d-flex">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-light" type="submit"> 
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-search"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
         </button>
       </form>
     </div>
--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -4,10 +4,10 @@ export const Contact = () => {
   return (
     <div className="container mb-4">
       <h3 className="text-center text-uppercase py-4 mt-4">Contact</h3>
-      <div className=" fst-italic pt-1">
+      <div className=" mx-auto fst-italic pt-1">
         <form>
           <div className="mb-3">
-            <label for="name" className="form-label">
+            <label htmlFor="name" className="form-label">
               Fullname
             </label>
             <input
@@ -18,7 +18,7 @@ export const Contact = () => {
             />
           </div>
           <div className="mb-3">
-            <label for="phone" className="form-label">
+            <label htmlFor="phone" className="form-label">
               Phone
             </label>
             <input
@@ -29,7 +29,7 @@ export const Contact = () => {
             />
           </div>
           <div className="mb-3">
-            <label for="email" className="form-label">
+            <label htmlFor="email" className="form-label">
               Email address
             </label>
             <input
@@ -43,7 +43,7 @@ export const Contact = () => {
             </div>
           </div>
           <div className="mb-4">
-            <label for="email" className="form-label">
+            <label htmlFor="email" className="form-label">
               Email address
             </label>
             <input
@@ -54,25 +54,25 @@ export const Contact = () => {
             />
           </div>
           <div className="mb-3 pt-4 pb-3">
-            <select class="form-select" aria-label="Default select example">
-              <option selected>cuando podemos contactarte</option>
+            <select className="form-select" aria-label="Default select example">
+              <option defaultValue="">cuando podemos contactarte</option>
               <option value="M">Mañana</option>
               <option value="T">Tarde</option>
               <option value="N">Noche</option>
             </select>
           </div>
           <div className="mb-4 pt-4 pb-3">
-            <div class="form-floating">
+            <div className="form-floating">
               <textarea
-                class="form-control"
+                className="form-control"
                 placeholder="Leave a comment here"
                 id="floatingTextarea"
               ></textarea>
-              <label for="floatingTextarea">Comments</label>
+              <label htmlFor="floatingTextarea">Comments</label>
             </div>
           </div>
           <div className="pb-4 mt-4 d-grid">
-            <button type="submit" className="btn btn-primary ">
+            <button type="submit" className="btn btn-secondary">
               Submit
             </button>
           </div>
