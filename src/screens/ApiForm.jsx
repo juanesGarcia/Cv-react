5899c364154791150e154e0eae0618e300dbf63f
juanes <juanesgym2018@gmail.com>
2022-08-30 17:22:33 -0500

    pages

--- src/screens/ApiForm.jsx
+++ src/screens/ApiForm.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { useState } from "react";
 
-export const ApiForm = ({createUser}) => {
+export const ApiForm = ({ createUser }) => {
+  const [user, setUser] = useState("");
+  const [city, setCity] = useState("");
 
-    const [user,setUser]= useState();
-    const [city,setCity]= useState();
-
-    const handlesubmit =(e)  => { 
-        e.preventDefault();
-        console.log(user);
-      
-        createUser(user,city);
-     };    
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    
+      createUser(user, city);
+    
+    setCity("");
+    setUser("");
+  };
   return (
     <div className="mb-4 pb-2 border-bottom border-Dark border- Solid">
       <form onSubmit={handlesubmit}>
@@ -23,10 +25,12 @@ export const ApiForm = ({createUser}) => {
             type="text"
             className="form-control"
             id="exampleFormControlInput1"
-            placeholder=""
-            onChange={(e)=>{
-            setUser(e.target.value);
+            placeholder="escribe tu nombre"
+            value={user}
+            onChange={(e) => {
+              setUser(e.target.value ? e.target.value:"");
             }}
+            autoFocus
           />
         </div>
         <div className="m-4">
@@ -37,13 +41,16 @@ export const ApiForm = ({createUser}) => {
             className="form-control"
             id="exampleFormControlTextarea1"
             rows="3"
+            placeholder="escribe tu cuidad"
+            value={city}
             onChange={(e) => {
-                setCity(e.target.value);
+              setCity(e.target.value);
             }}
           ></textarea>
-
         </div>
-        <button type="submit" className="btn btn-dark m-4">Submit</button>
+        <button type="submit" className="btn btn-dark m-4">
+          Submit
+        </button>
       </form>
     </div>
   );
