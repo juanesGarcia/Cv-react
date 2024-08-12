2e31ab56bb157a85843023d479dc3e3779e4990e
juanes <67403119+juanesGarcia@users.noreply.github.com>
2024-08-12 16:29:38 -0500

    agregando nuevos proyectos

--- src/screens/MyPro.jsx
+++ src/screens/MyPro.jsx
@@ -3,6 +3,10 @@ import par from '../img/parqueadero.jpg'
 import fit from '../img/fit.jpg';
 import imgapi from '../img/api.png';
 import logofinal from '../img/logofinal.jpg';
+import col from '../img/colombia.png';
+import perfile from '../img/perfile.jpeg';
+import practicante from '../img/practicante.jpeg';
+import marca from '../img/marca.jpeg';
 import { Cproyect } from "./Cproyect";
 import "../Style/MyProStyle.css";
 import * as pr from "./InfoProyect";
@@ -22,19 +26,37 @@ export const MyPro = () => {
       <div className="conc flex-fill mb-4 mt-4 " data-aos="zoom-in-up">      
       <Cproyect img={logofinal}  but={pr.but} info={pr.textvi} title={pr.titlevi} li={pr.urlvi} ></Cproyect>
       </div>
-      <div className="conc flex-fill mb-4 mt-4 " data-aos="zoom-in-up">
-        <Cproyect  img={fit} info={pr.textfit} but={pr.but} title={pr.titlefit} li={pr.urlfit}></Cproyect>
+      <div className="conc flex-fill mb-4 mt-4 " data-aos="zoom-in-up">      
+      <Cproyect img={col}  but={pr.but} info={pr.textfrontendjr} title={pr.titlefrontendjr} li={pr.urlfrontendjr} ></Cproyect>
       </div>
     </div>
-    <div className="pro2 d-flex ">
+    <div className="pro2 d-flex ">  
       <div className="conc flex-fill mb-4 mt-4 pb-4" data-aos="zoom-in-up">      
-        <Cproyect img={imgapi} but={pr.but} info={pr.textapi} title={pr.titleapi} li={pr.urlapi} p={pr.p}></Cproyect>
+        <Cproyect img={perfile} but={pr.but} info={pr.textperfiles} title={pr.titleperfiles} li={pr.urlperfiles} p={pr.p}></Cproyect>
+      </div>
+      <div className="conc flex-fill mb-4 mt-4 pb-4" data-aos="zoom-in-up">      
+        <Cproyect img={fit} but={pr.but} info={pr.textfit} title={pr.titlefit} li={pr.urlfit} p={pr.p}></Cproyect>
+      </div>
+    
+
+    </div>
+    <div className="pro2 d-flex ">
+    <div className="conc flex-fill mb-4 mt-4 pb-4" data-aos="zoom-in-up">      
+        <Cproyect img={practicante} but={pr.but} info={pr.textpracticante} title={pr.titlepracticante} li={pr.urlpracticantes} p={pr.p}></Cproyect>
       </div>
       <div className="conc flex-fill mb-4 mt-4 pb-4 " data-aos="zoom-in-up" >
         <Cproyect img={par} info={pr.textp} but={pr.but} title={pr.titlep} li={pr.urlp}></Cproyect>
       </div>
     </div>
+    <div className="pro2 d-flex ">
+    <div className="conc flex-fill mb-4 mt-4 pb-4" data-aos="zoom-in-up">      
+        <Cproyect img={imgapi} but={pr.but} info={pr.textapi} title={pr.titleapi} li={pr.urlapi} p={pr.p}></Cproyect>
+      </div>
+      <div className="conc flex-fill mb-4 mt-4 pb-4" data-aos="zoom-in-up">      
+        <Cproyect img={marca} but={pr.but} info={pr.textia} title={pr.titleia} li={pr.urlia} p={pr.p}></Cproyect>
+      </div>
     </div>
+</div>
   
     
   );
